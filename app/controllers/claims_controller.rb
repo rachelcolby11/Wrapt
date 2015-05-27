bd2fdb5a5c6611822d20d32caa73460d6c4c9af9
Rachel Colby <rachelcolby11@gmail.com>
2015-05-27 13:26:41 -0700

    Complete Claim functionality

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,21 +1,30 @@
 class ClaimsController < ApplicationController
- 
+
   def create
      @item = Item.find(params[:item_id])
-     @claim = current_user.build_claim(item: @item)
+     @claim = Claim.create(user: current_user, item: @item)
  
       if @claim.save 
-       flash[:notice] = "You have claimed\"#{@item.name}\". Make sure to un-claim this item should anything change."
-       redirect_to [@item]
+       flash[:notice] = "You have claimed \"#{@item.name}\". Make sure to unclaim this item should anything change."
+       redirect_to @item
      else
        flash[:error] = "There was an error claiming this item. Please try again."
-       redirect_to [@item]
+       redirect_to @item
      end
    end
 
 
   def destroy
+    @item = Item.find(params[:item_id])
+    @claim = Claim.find(params[:id])
 
+    if @claim.destroy
+       flash[:notice] = "You have unclaimed \"#{@item.name}\"."
+       redirect_to @item
+     else
+       flash[:error] = "There was an error. Please try again."
+       redirect_to @item
+     end
   end
 
 end
