251fafcbe071bdad873e961a31413140c1f846d1
Rachel Colby <rachelcolby11@gmail.com>
2015-06-02 11:14:14 -0700

    refactor of friendships

--- db/migrate/20150528043110_create_friendships.rb
+++ db/migrate/20150528043110_create_friendships.rb
@@ -1,12 +1,14 @@
 class CreateFriendships < ActiveRecord::Migration
+  class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
       t.references :user, index: true
-      t.references :buddy, index: true
+      t.integer :buddy_id
 
       t.timestamps null: false
     end
     add_foreign_key :friendships, :users
-    add_foreign_key :friendships, :buddies
+    add_index :friendships, :buddy_id
   end
+
 end
