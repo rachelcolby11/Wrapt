04f925e056afb66b382c4c0a9d71b91f4389fa8e
Rachel Colby <rachelcolby11@gmail.com>
2015-06-07 23:40:49 -0700

    Link to restrict viewers WIP

--- app/controllers/exclusions_controller.rb
+++ app/controllers/exclusions_controller.rb
@@ -2,10 +2,12 @@ class ExclusionsController < ApplicationController
 
   def create
      @item = Item.find(params[:item_id])
-     @exclusion = Exclusion.create(user: buddy, item: @item)
+     @user = current_user
+
+     @exclusion = Exclusion.create(user: @user.buddy, item: @item)
  
       if @exclusion.save 
-       flash[:notice] = "You have restricted \"#{buddy.name}\"" from viewing \"#{@item.name}\""
+       flash[:notice] = "You have restricted \"#{buddy.name}\" from viewing \"#{@item.name}\""
        redirect_to restrictions_item_path
      else
        flash[:error] = "There was an error. Please try again."
