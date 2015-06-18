d8e45df200d9314f7461a1e314004192c12350ff
Rachel Colby <rachelcolby11@gmail.com>
2015-06-17 21:03:13 -0700

    Configured for Production emails

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
