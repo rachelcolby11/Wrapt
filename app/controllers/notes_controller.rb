31afaa054d2d3a5d74f8d14aa9ec0bcdb9630bb8
Rachel Colby <rachelcolby11@gmail.com>
2015-08-10 23:24:46 -0700

    Create Notes form and New & Create actions; update routes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,28 @@
 class NotesController < ApplicationController
 
+  def new
+    @note = Note.new
+  end
+
   def create
+    @note = current_user.notes.build(note_params)
+     if @note.save
+       flash[:notice] = "Your note was saved."
+       redirect_to dashboard_path
+     else
+       flash[:error] = "There was an error saving your note. Please try again."
+       render :new
+     end
+  end
+
+  def edit
+
   end
 
   def destroy
+  end
+
+  def note_params
+    params.require(:note).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :user_profiles
 
+  resources :notes
+
   namespace :admin do
     resources :users
   end
